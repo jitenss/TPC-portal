b6d49651832e7a051d05a8c28d0126372a90014c
jitenss <sadhwani.jiten@gmail.com>
2017-06-09 17:18:33 +0530

    Add the student detail model and its migration

---
+++ db/migrate/20170609104811_create_student_details.rb
@@ -0,0 +1,27 @@
+class CreateStudentDetails < ActiveRecord::Migration
+  def change
+    create_table :student_details do |t|
+    	t.string "s_name" , :null=> false
+    	t.integer "batch", :null=> false
+    	t.string "roll_no", :null=> false
+    	t.string "branch", :null=> false
+    	t.string "gender", :null=> false
+    	t.date "date_of_birth", :null=> false
+    	t.integer "contact_no_1",:null=> false, :limit=> 10
+    	t.integer "contact_no_2", :limit=> 10
+    	t.string "email", :null=> false
+    	t.float "spi_1", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_2", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_3", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_4", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_5", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_6", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "spi_7", :precision=> 4, :scale=> 2
+    	t.float "cpi_6", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "cpi_7", :precision=> 4, :scale=> 2
+    	t.float "10_percent", :null=> false, :precision=> 4, :scale=> 2
+    	t.float "12_percent", :null=> false, :precision=> 4, :scale=> 2
+      	t.timestamps :null=> false
+    end
+  end
+end
