ac04c684d3a84605c4e10a4cd8044c92563dc679
Meetasha29 <meetaasha29@gmail.com>
2017-06-11 12:03:51 +0530

    add index and foreign key to student database and add cin

--- db/migrate/20170609125501_create_companies.rb
+++ db/migrate/20170609125501_create_companies.rb
@@ -1,6 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
+      t.string "cin", :null=>false
       t.string "c_name", :null=> false
       t.float "cpiCriteria", :null=> false, :precision=> 4, :scale=> 2
       t.string "category", :null=> false
@@ -10,5 +11,6 @@ class CreateCompanies < ActiveRecord::Migration
       t.text "skills_set"
       t.timestamps null: false
     end
+    add_index("companies","cin")
   end
 end
--- db/migrate/20170610013900_create_student_registers.rb
+++ db/migrate/20170610013900_create_student_registers.rb
@@ -1,11 +1,21 @@
 class CreateStudentRegisters < ActiveRecord::Migration
-  def change
+  def up
     create_table :student_registers do |t|
-    	t.references :student_details
-    	t.references :companies
+    	t.references :student_detail
+    	t.references :company
       	t.timestamps null: false
     end
+    add_foreign_key :student_registers , :student_details,on_delete: :cascade,on_update: :cascade
+    add_foreign_key :student_registers , :companies,on_delete: :cascade,on_update: :cascade
     add_index("student_registers","student_detail_id")
     add_index("student_registers","company_id")
   end
+  def down
+    remove_foreign_key :student_registers, :student_details
+    remove_foreign_key :student_registers, :companies
+    remove_index :student_registers, :student_detail_id
+    remove_index :student_registers, :company_id
+    drop_table :student_registers
+  end
+
 end
